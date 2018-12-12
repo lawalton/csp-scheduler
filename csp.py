318a60b94060988b24579ecc311f25e146f7db58
lawalton <liwa9544@colorado.edu>
2018-12-12 11:26:22 -0700

    Now checks for time conflicts.

--- csp.py
+++ csp.py
@@ -6,6 +6,7 @@ class CSP:
         self.courses = courses
         self.rooms = rooms
         self.num_backtrack = 0
+        self.cost = 0
 
 """
 considerations for the final exam scheduler:
@@ -39,17 +40,32 @@ def check_constraints(course, final_assignment, csp, assignment):
     room_occupancy = csp.rooms[room_name]
     num_students = csp.courses[course][2]
 
+    if room_occupancy < num_students:
+        return False
+
+    course_day = csp.courses[course][0]
+    course_time = csp.courses[course][1]
+    final_assignment_day = final_assignment[0]
+    final_assignment_time = final_assignment[1]
+
     # check if that room and time is already reserved
     for course_assigned in assignment:
         # that slot and room has already been taken
         if final_assignment == assignment[course_assigned]:
             return False
 
-    if room_occupancy < num_students:
-        return False
-                
-    # check for time conflicts
+        # check for time conflicts
+        assigned_course_day = csp.courses[course_assigned][0]
+        assigned_course_time = csp.courses[course_assigned][1]
+        assigned_final_day = assignment[course_assigned][0]
+        assigned_final_time = assignment[course_assigned][1]
 
+        if (final_assignment_day ==  assigned_final_day) and (final_assignment_time == assigned_final_time):
+            # two finals were scheduled at the same time
+            # this can only happen if the regular class periods are at the same time
+            if ((course_day !=  assigned_course_day) or (course_time != assigned_course_time)):
+                return False
+        
     return True
     
 def find_next_node(csp, assignment):
@@ -60,7 +76,7 @@ def find_next_node(csp, assignment):
     
 def recursive_backtracking(assignment, csp):
     # Your code here #
-
+    csp.cost = csp.cost + 1
     if len(assignment) == len(csp.courses):
         return assignment
 
@@ -75,12 +91,15 @@ def recursive_backtracking(assignment, csp):
                 return result
             else:
                 del assignment[course]
+                csp.num_backtrack = csp.num_backtrack + 1
     return False
 
 # create the possible domains for monday - friday, 8am - 5pm, two hour block
-days = ["M", "T", "W", "R", "F"]
-times = [8, 10, 12, 2, 4]
-rooms = {"ECEE 156": 130, "MATH 100": 300, "CHEM 140": 400, "HLMS 70": 150}
+#days = ["M", "T", "W", "R", "F"]
+days = ["M"]
+#times = [8, 10, 12, 2, 4]
+times = [8, 10, 12]
+rooms = {"CHEM 140": 400, "ECEE 156": 130, "MATH 100": 250, "HLMS 70": 150}
 domain = []
 for day in days:
     for time in times:
@@ -90,14 +109,17 @@ for day in days:
 # for now, assuming classes are mwf, and tr
 # m designates mwf and t designates t/r
 courses = OrderedDict([
-    ("CSCI 1300", ["M", 1, 300]),
-    ("CSCI 2270", ["T", 12, 350]),
     ("CSCI 3202", ["M", 4, 70]), 
+    ("CSCI 1300", ["M", 1, 170]),
+    ("CSCI 2270", ["T", 12, 350]),
     ("APPM 1400", ["M", 1, 130]),
-    ("APPM 1300", ["T", 2, 200])
+    ("APPM 1300", ["T", 12, 200])
  ])
 
 csp = CSP(courses, rooms, domain)
 a = backtracking_search(csp)
 print("=================")
 print(a)
+print(csp.num_backtrack)
+print(csp.cost)
+
